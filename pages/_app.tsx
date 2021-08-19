1a6f9d19e568b8a263dae73746b30aeeb79e26cc
Rod Joseph <rodjoseph98@gmail.com>
2021-08-19 13:59:51 -0400

    supabase ui and key

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
-import * as React from 'react';
-import Head from 'next/head';
-import { AppProps } from 'next/app';
-import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { CacheProvider, EmotionCache } from '@emotion/react';
-import theme from '../src/theme';
-import createEmotionCache from '../src/createEmotionCache';
-
+import * as React from "react";
+import Head from "next/head";
+import { AppProps } from "next/app";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { CacheProvider, EmotionCache, ThemeProvider } from "@emotion/react";
+import theme from "../src/theme";
+import createEmotionCache from "../src/createEmotionCache";
+import { Auth } from "@supabase/ui";
+import { supabase } from "../constants/supabase";
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
@@ -17,16 +17,18 @@ interface MyAppProps extends AppProps {
 export default function MyApp(props: MyAppProps) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <title>My page</title>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </CacheProvider>
+    <Auth.UserContextProvider supabaseClient={supabase}>
+      <CacheProvider value={emotionCache}>
+        <Head>
+          <title>My page</title>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </Head>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
+    </Auth.UserContextProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,20 +2,66 @@ import * as React from "react";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
 import ProTip from "../src/ProTip";
 import Link from "../src/Link";
 import Copyright from "../src/Copyright";
+import BookingStepper from "../components/newbooking";
+import { Auth } from "@supabase/ui";
+import {
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+  TextField
+} from "@material-ui/core";
 
-export default function Index() {
+export default function Booking() {
+  const [timePickersVibible, setTimePickersVisible] = React.useState(false);
+  const [startDate, setStartDate] = React.useState(false);
+  const [endDate, setEndDate] = React.useState(false);
+  const [startTime, setStartTime] = React.useState(false);
+  const [endTime, setEndTime] = React.useState(false);
+
+  const { user } = Auth.useUser();
+  if (!user) return <Auth supabaseClient={supabase} />;
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
           Next.js v5-beta with TypeScript example
         </Typography>
-        <Link href="/booking" color="secondary">
-          Go to the booking page
-        </Link>
+        <Button variant="contained" component={Link} noLinkStyle href="/">
+          Go to the main page
+        </Button>
+        <form noValidate>
+          <TextField
+            id="datetime-local"
+            label="Next appointment"
+            type="datetime-local"
+            InputLabelProps={{
+              shrink: true
+            }}
+          />
+          <TextField
+            id="datetime-local"
+            label="Next appointment"
+            type="date"
+            InputLabelProps={{
+              shrink: true
+            }}
+          />
+          <FormControl component="fieldset">
+            <FormLabel component="legend">Truck Type</FormLabel>
+            <RadioGroup aria-label="truck-type" name="trucktype1">
+              <FormControlLabel value="1" control={<Radio />} label="Type 1" />
+              <FormControlLabel value="2" control={<Radio />} label="Type 2" />
+              <FormControlLabel value="3" control={<Radio />} label="Type 3" />
+              <FormControlLabel value="4" control={<Radio />} label="Type 4" />
+            </RadioGroup>
+          </FormControl>
+        </form>
         <ProTip />
         <Copyright />
       </Box>
